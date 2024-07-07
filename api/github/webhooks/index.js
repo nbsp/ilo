bd9b1004967b446aee7dce359afb447a7838e10d
aoife cassidy <aoife@enby.space>
2024-07-06 18:20:11 -0700

    fix vercel

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,6 +1,6 @@
 import { createNodeMiddleware, createProbot } from "probot";
 
-import app from "../../..";
+import app from "../../../index.js";
 const probot = createProbot();
 
 export default createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
